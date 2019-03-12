adc9e568a57bc683b4118af71dc8ea445b682133
Columbus Instructor <instructor@wecancodeit.org>
2019-03-12 11:54:27 -0400

    Add JSgit add .git add .git add .!

---
+++ public/js/app.js
@@ -0,0 +1,49 @@
+// document.getElementById
+// document.getElementsByClassName
+// document.getElementsByTagName
+
+let title = 'title';
+const articleDeleteButtons = document.querySelectorAll('.delete-article');
+const articleToggleButton = document.querySelector('.hide-articles')
+
+var abc = "doesn't matter"
+
+// Able to change this value
+title = document.querySelector('h1');
+
+// this doesn't work
+// buttons = "Something else";
+
+// Manipulate something about the title
+title.textContent = 'Some new value';
+
+// Events
+function deleteArticles() {
+    articleDeleteButtons.forEach(function foo(button) {
+        button.addEventListener('click', function onButtonClick() {
+            const articleToDelete = this.parentElement.parentElement
+            // console.log(articleToDelete)
+            console.log(articleToDelete.remove())
+        })
+    })
+}
+
+articleToggleButton.addEventListener('click', function toggle() {
+    const articleContainer = document.querySelector('.article-container')
+
+    // Don't do this because of separation of concerns
+    //
+    // if (articleContainer.style.display == 'grid') {
+    //     articleContainer.style.display = 'none'
+    // } else {
+    //     articleContainer.style.display = 'grid'
+    // }
+
+    articleContainer.classList.toggle('hide')
+})
+
+function run() {
+    deleteArticles()
+}
+
+run()
